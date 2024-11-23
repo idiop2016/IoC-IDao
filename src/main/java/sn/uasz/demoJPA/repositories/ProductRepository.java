3dd0a05b63245d0b6c3e9901fd9fa3e3fd79f717
Ibrahima DIOP <ibrahima.diop@univ-zig.sn>
2024-11-23 10:55:20 +0000

    ajout de methode dans ProduitRepository

--- src/main/java/sn/uasz/demoJPA/repositories/ProductRepository.java
+++ src/main/java/sn/uasz/demoJPA/repositories/ProductRepository.java
@@ -8,7 +8,7 @@ import sn.uasz.demoJPA.entities.Product;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-   // public List<Product> findByNameContainsIgnoreCase(String kw);
-    //@Query("select p from Product p where p.name like %:ac% and p.price>:p")
-    //public List<Product> search(@Param("ac") String kw, @Param("p")double price);
+   public List<Product> findByNameContainsIgnoreCase(String kw);
+   @Query("select p from Product p where p.name like %:ac% and p.price>:p")
+   public List<Product> search(@Param("ac") String kw, @Param("p")double price);
 }
