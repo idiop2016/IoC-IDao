e9206bbdeb08ab6aff60e9f25f7af4d840e99c74
Ibrahima DIOP <ibrahima.diop@univ-zig.sn>
2024-11-23 19:46:24 +0000

    version apres compréhension du format des noms des méthodes dans l'interface JPARepository

--- src/main/java/sn/uasz/demoJPA/DemoJpaApplication.java
+++ src/main/java/sn/uasz/demoJPA/DemoJpaApplication.java
@@ -4,28 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import sn.uasz.demoJPA.entities.Enseignant;
+import sn.uasz.demoJPA.entities.Enseignant;
 import sn.uasz.demoJPA.entities.Product;
-//import sn.uasz.demoJPA.repositories.EnseignantRepository;
+import sn.uasz.demoJPA.repositories.EnseignantRepository;
 import sn.uasz.demoJPA.repositories.ProductRepository;
 
 import java.util.List;
 
 @SpringBootApplication
 public class DemoJpaApplication implements CommandLineRunner {
-
+	@Autowired
 	ProductRepository productRepository;
+	@Autowired
+	EnseignantRepository enseignantRepository;
 
-	public DemoJpaApplication(ProductRepository productRepository) {
+	/*public DemoJpaApplication(ProductRepository productRepository) {
 		this.productRepository = productRepository;
-	}
+	}*/
 	//EnseignantRepository enseignantRepository;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoJpaApplication.class, args);
 	}
-
+/*
 	@Override
 	public void run(String... args) {
 
@@ -33,28 +35,29 @@ public class DemoJpaApplication implements CommandLineRunner {
 		productRepository.save(new Product(null, "scanner",120000,12));
 		productRepository.save(new Product(null, "imprimante",150000,40));
 
-		/*
+
 		List<Product> products = productRepository.findAll("imp");
 		List<Product> products = productRepository.findByNameContains("imp");
 		List<Product> products = productRepository.findByNameContainsIgnoreCase("IMP");
-		*/
 
-		List<Product> products = productRepository.findAll();
+		//List<Product> products = productRepository.findByNameContainsIgnoreCase("IMP");
+		//List<Product> products = productRepository.findAll();
+		//List<Product> products = productRepository.search("imp",120000);
 		products.forEach(product -> {
 			System.out.println("--------------------");
 			System.out.println(product.getName());
 			System.out.println(product.getPrice());
 			System.out.println(product.getQuantity());
 		});
-	}
-	/*
+	}*/
+
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 
 		enseignantRepository.save(new Enseignant(null,"Diop","Ibrahima","181185/D","Professeur Assimulé"));
 		enseignantRepository.save(new Enseignant(null,"Fall","Modou","181186/A","Vacataire"));
 		enseignantRepository.save(new Enseignant(null,"Ndiaye","Ibrahima","181187/B","Vacataire"));
-		List<Enseignant> enseignents = enseignantRepository.findByNameContainsIgnoreCase("Ibrahim");
+		List<Enseignant> enseignents = enseignantRepository.findByPrenomContainsIgnoreCase("Ibra");
 		enseignents.forEach(enseignant -> {
 			System.out.println("--------------------");
 			System.out.println(enseignant.getPrenom());
@@ -62,6 +65,6 @@ public class DemoJpaApplication implements CommandLineRunner {
 			System.out.println(enseignant.getMatricule());
 			System.out.println(enseignant.getGrade());
 		});
-	}*/
+	}
 }
 
--- src/main/java/sn/uasz/demoJPA/repositories/EnseignantRepository.java
+++ src/main/java/sn/uasz/demoJPA/repositories/EnseignantRepository.java
@@ -2,9 +2,11 @@ package sn.uasz.demoJPA.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sn.uasz.demoJPA.entities.Enseignant;
+import sn.uasz.demoJPA.entities.Product;
 
 import java.util.List;
 
 public interface EnseignantRepository extends JpaRepository<Enseignant,Long> {
-    public List<Enseignant> findByNameContainsIgnoreCase(String kw);
+    //public List<Enseignant> findByNameContainsIgnoreCase(String kw);
+    public List<Enseignant> findByPrenomContainsIgnoreCase(String kw);
 }
